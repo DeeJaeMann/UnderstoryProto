7a7948df8f07b08928a5b1180be6cf2b545ebd69
Dee Jae Mann <dee.j.mann@hotmail.com>
2024-07-24 17:48:29 -0400

    Added slow effect to web

---
+++ Assets/Scripts/SpiderWebBehavior.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class SpiderWebBehavior : MonoBehaviour
+{
+    public float slowModifier = .5f;
+    private PlayerBehavior player;
+    private float playerBaseSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<PlayerBehavior>();
+        playerBaseSpeed = player.speed;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            player.speed *= slowModifier;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    { 
+        if (collision.gameObject.tag == "Player")
+        {
+            player.speed = playerBaseSpeed;
+        }
+    }
+}
